5688e2a786d22fca91924103c8ea9a4e32d80da4
maart <maart@10.150.142.242>
2019-05-13 13:10:51 +0200

    quiz 4 door maarten

---
+++ src/hoofdstuk11/quiz4/SuperA.java
@@ -0,0 +1,12 @@
+package hoofdstuk11.quiz4;
+
+public class SuperA extends SuperB
+{
+	int y = 7;
+	
+	public SuperA()
+	{
+		y ++;
+		System.out.print(" y" + y);
+	}
+}
---
+++ src/hoofdstuk11/quiz4/SuperB.java
@@ -0,0 +1,11 @@
+package hoofdstuk11.quiz4;
+
+public class SuperB {
+	protected int x = 3;
+	
+	public SuperB()
+	{
+		x += 2;
+		System.out.print(" x" + x);
+	}
+}
