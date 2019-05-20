731af12347f74f63a91059ef16aa4e78d577e8fc
Jelle Kuiper <39058012+JelleKuiper@users.noreply.github.com>
2019-05-20 13:39:01 +0200

    H12 quiZ 3

---
+++ src/hoofdstuk12/quiz3/Subklasse.java
@@ -0,0 +1,15 @@
+package hoofdstuk12.quiz3;
+
+public class Subklasse extends Superklasse {
+	int x = 3;
+	String methodeA(String s, int i) {
+		String str = s + ", " + i;
+		//Opdracht: Welke foutmelding komt er met dit: super.x ++;
+		//The final field Superklasse.x cannot be assigned.
+		return str;
+	}
+	public static void main(String[] args) {
+		Subklasse sk = new Subklasse();
+		System.out.println(sk.methodeA(23, "Emma"));
+	}
+}
---
+++ src/hoofdstuk12/quiz3/Superklasse.java
@@ -0,0 +1,10 @@
+package hoofdstuk12.quiz3;
+
+public class Superklasse {
+	final int x = 4;
+	
+	final String methodeA(int i, String s) {
+		String str = i + ", " + s;
+		return str;
+	}
+}
