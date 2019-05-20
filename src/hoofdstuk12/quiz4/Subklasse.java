4442b29c96ae1f4e91ffba75d76feb6a8281f85c
Jelle Kuiper <39058012+JelleKuiper@users.noreply.github.com>
2019-05-20 13:49:04 +0200

    h12 quiz4

---
+++ src/hoofdstuk12/quiz4/Subklasse.java
@@ -0,0 +1,15 @@
+package hoofdstuk12.quiz4;
+
+public class Subklasse extends Superklasse{
+	void methodeA(int x, int y) {
+		int z = x - y;
+		System.out.print(z);
+	}
+	public static void main(String[] args) {
+		Subklasse sk = new Subklasse();
+		//Opdracht wat gebeurt er als je (5, 3.0) veranderd naar 6 en 4?
+		//Dan neemt ie de methode van de subklasse niet de superklasse 
+		//omdat het allebij ints zijn niet doubles..
+		sk.methodeA(6, 4);
+	}
+}
---
+++ src/hoofdstuk12/quiz4/Superklasse.java
@@ -0,0 +1,8 @@
+package hoofdstuk12.quiz4;
+
+public class Superklasse {
+	final void methodeA(int x, double y) {
+		double z = x * y;
+		System.out.print(z);
+	}
+}
