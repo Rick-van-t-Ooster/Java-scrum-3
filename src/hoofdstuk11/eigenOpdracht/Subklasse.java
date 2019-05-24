df05ba09473a4ca7bf63f90e75987fa934246e12
Jelle Kuiper <39058012+JelleKuiper@users.noreply.github.com>
2019-05-24 13:22:57 +0200

    eigen opdracht h11 mijn deel

--- src/hoofdstuk11/eigenOpdracht/Subklasse.java
+++ src/hoofdstuk11/eigenOpdracht/Subklasse.java
@@ -1,5 +1,29 @@
 package hoofdstuk11.eigenOpdracht;
 
-public class Subklasse {
+import java.util.Scanner;
 
+public class Subklasse extends Superklasse {
+	protected static int aantalLiterBenzine = 30;
+	Subklasse(int aantalLiterBenzine){
+		super(30);
+	}
+	
+	//Deze methode door Jelle
+	public static void rijden() {
+		System.out.println("Deze auto rijdt 30 kilometer op 1 liter benzine, hij heeft 30 liter benzine.");
+		System.out.println("Hoeveel kilometer wilt u rijden?");
+		Scanner scanner = new Scanner(System.in);
+		int input = scanner.nextInt();
+		if (input > 900) {
+			System.out.println("Zo ver kan de auto niet rijden!");
+		}else {
+			int kmEraf = input / 30;
+			aantalLiterBenzine = aantalLiterBenzine - kmEraf;
+			System.out.println("U heeft " + input + " kilometer gereden, daarom is er nu " + aantalLiterBenzine + " liter over.");
+		}
+	}
+	
+	public static void main(String[] args) {
+		rijden();
+	}
 }
--- src/hoofdstuk11/eigenOpdracht/Superklasse.java
+++ src/hoofdstuk11/eigenOpdracht/Superklasse.java
@@ -1,5 +1,8 @@
 package hoofdstuk11.eigenOpdracht;
 
 public class Superklasse {
-
+	protected int aantalLiterBenzine = 30;
+	Superklasse(int aantalLiterBenzine){
+		
+	}
 }
