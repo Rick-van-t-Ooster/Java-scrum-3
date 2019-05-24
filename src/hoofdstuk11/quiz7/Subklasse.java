ecbcb3d6267042ddd050f614414b6ca034ba304d
Jelle Kuiper <39058012+JelleKuiper@users.noreply.github.com>
2019-05-24 12:46:09 +0200

    quiz 7 h 11 door jelle

--- src/hoofdstuk11/quiz7/Subklasse.java
+++ src/hoofdstuk11/quiz7/Subklasse.java
@@ -1,7 +1,6 @@
 package hoofdstuk11.quiz7;
-
+//Gemaakt door jelle
 public class Subklasse extends Superklasse{
-<<<<<<< HEAD
 	Subklasse(String naam) {
 		//Vraag 1 en 2, wat gebeurd er als je super(naam) veranderd met super("s")?
 		super("s");
@@ -10,16 +9,4 @@ public class Subklasse extends Superklasse{
 	public static void main(String[] args) {
 		Subklasse sk = new Subklasse("x");
 	}
-=======
-
-	Subklasse(String naam) {
-		super(naam);
-		System.out.print(naam);
-	}
-	
-	public static void main(String[] args) {
-		Subklasse sk = new Subklasse("x");
-	}
-
->>>>>>> feb165eee9818a56c765fec91dbe2ef229079179
 }
--- src/hoofdstuk11/quiz7/Superklasse.java
+++ src/hoofdstuk11/quiz7/Superklasse.java
@@ -1,21 +1,11 @@
 package hoofdstuk11.quiz7;
-
+//Gemaakt door jelle
 public class Superklasse {
-<<<<<<< HEAD
 	Superklasse(String naam){
 		this(naam, "d");
 		System.out.print(naam);
 	}
-	Superklasse(String naam, String naam2){
-=======
-
-	Superklasse(String naam) {
-		this(naam, "d");
-		System.out.print(naam);;
-	}
-
-	public Superklasse(String naam, String naam2) {
->>>>>>> feb165eee9818a56c765fec91dbe2ef229079179
+	Superklasse(String naam, String naam2) {
 		System.out.print(naam);
 		System.out.print(naam2);
 	}
