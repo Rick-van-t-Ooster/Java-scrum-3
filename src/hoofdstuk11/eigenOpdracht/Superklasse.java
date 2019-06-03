04fea9bca4d0505cc92f473fd2e29df008ec8139
Maarten2403 <Maartenvandenberg2403@gmail.comgit config --global user.email Maartenvandenberg2403@gmail.comgit config --global user.email Maartenvandenberg2403@gmail.com>
2019-06-03 14:01:04 +0200

    methode toegevoegt

--- src/hoofdstuk11/eigenOpdracht/Superklasse.java
+++ src/hoofdstuk11/eigenOpdracht/Superklasse.java
@@ -2,7 +2,14 @@ package hoofdstuk11.eigenOpdracht;
 
 public class Superklasse {
 	protected int aantalLiterBenzine = 30;
-	Superklasse(int aantalLiterBenzine){
-		
+	//method maarten
+	protected char liter = 'L';
+	Superklasse(int aantalLiterBenzine) {
+	}
+	//method maarten
+	void mijnMethode(){
+		aantalLiterBenzine += 4;
+		System.out.print(liter);
+		System.out.print(aantalLiterBenzine);
 	}
 }
