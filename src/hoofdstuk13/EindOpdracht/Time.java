3e1570c6dfb3e557613a6ba802d02944d752d986
Jelle Kuiper <39058012+JelleKuiper@users.noreply.github.com>
2019-06-14 13:09:50 +0200

    eigenOpdracht h13

--- src/hoofdstuk13/EindOpdracht/Time.java
+++ src/hoofdstuk13/EindOpdracht/Time.java
@@ -3,7 +3,6 @@ package hoofdstuk13.EindOpdracht;
 import java.util.Calendar;
 
 public class Time {
-
 	public static void main(String[] args) {
 		Calendar c = Calendar.getInstance();
 		System.out.println(c.getTime());
