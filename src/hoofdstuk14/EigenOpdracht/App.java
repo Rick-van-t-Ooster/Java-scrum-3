11584157277bda78e51bdf4ef965f349c5eb05cc
Jelle Kuiper <39058012+JelleKuiper@users.noreply.github.com>
2019-06-21 13:28:59 +0200

    Eigen Methode H14 Jelle

--- src/hoofdstuk14/EigenOpdracht/App.java
+++ src/hoofdstuk14/EigenOpdracht/App.java
@@ -5,12 +5,19 @@ public class App {
 	public static void main(String[] args) {
 		Rectangle rec = new Rectangle(2, 4, '\\', AnsiKleuren.ANSI_YELLOW, AnsiKleuren.ANSI_WHITE_BACKGROUND);
 		Triangle tri = new Triangle(5, '.', AnsiKleuren.ANSI_BLUE, AnsiKleuren.ANSI_BLUE_BACKGROUND);
+		Snake sn = new Snake(5, '+', AnsiKleuren.ANSI_RED, AnsiKleuren.ANSI_RED_BACKGROUND);
 		
 		tri.draw();
 		
 		System.out.println("Oppervlak driehoek: " + tri.area());
 		
 		rec.draw();
+		
+		//Jelles stuk (De slang is van jelle de rest van rick.)
+		System.out.println();
+		System.out.println("Lengte slang: " + sn.area());
+		
+		sn.draw();
 	}
 	
 }
